581e845be11b927c75d223a90f1898231a593053
Bubesh P <hwalk958@gmail.com>
2021-04-17 22:51:37 +0530

    Initial

---
+++ src/Home/home.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <div
+        style={{
+          marginTop: "3%",
+          fontWeight: "bold",
+          display: "inline-block",
+        }}
+      >
+        Hey! I'm <br />
+        <div style={{ fontSize: "80px" }}>Bubesh</div>
+        &nbsp; Software Engineer @ Accolite Digital
+      </div>
+      <hr />
+    </div>
+  );
+}
+
+export default Home;
